543d462d6246c4c4ecbcccd7ce6f905af7d35f7d
Kayla Nguyen <nguye27k@mtholyoke.edu>
2016-03-26 12:41:57 -0400

    finish checking for constraint violation

--- nonogram.py
+++ nonogram.py
@@ -22,37 +22,56 @@ def get_permutations():
 
 
 # check constrains for given row (board[row])
-# return number of constraint violated
+# return number of constraint violated, order matters
+# if there's more filled square in row than expected, each filled square is one violation
+# if there's more filled square in a sequence than expected, each extra filled square is a violation
 def check_constraint(row):
-    # print board[row]
+    print board[row]
     print ROWS[row]
 
-    # current_row = board[row]
-    current_row = [FILL, FILL, UNKNOWN, FILL, FILL, FILL, UNKNOWN]
-    counter = 0
+    current_row = board[row]
     violate = 0
-    print current_row
+    # print current_row
     for constraint in ROWS[row]:
         # flag to determine if the constraint is met
         flag = False
         for i in range(0, len(current_row)):
+            # print current_row
+            # print "looking at index " + str(i)
             if current_row[i] is FILL:
-                counter += 1
-                for j in range(i, len(current_row)):
+                counter = 1
+                for j in range(i+1, len(current_row)):
+                    counter += 1
+                    # print "checking " + str(j)
                     if current_row[j] is not FILL:
-                        print "current_row[i] is not fill at " + str(j)
+                        counter -= 1
+                        # print "current_row[i] is not fill at " + str(j)
+                        # print "counter is " + str(counter)
                         break
-            if counter == constraint:
-                print "Counter equals constrains"
-                # backtrack and remove that out of consideration
-                for a in range(0, constraint):
-                    current_row[i + a] = BLANK
-                flag = True
-                counter = 0
-                break
+                if counter == constraint:
+                    # print "Counter equals constrains"
+                    # backtrack and remove that out of consideration
+                    for a in range(0, constraint):
+                        # print "current row to delete is " + str(int(i+a))
+                        current_row[i + a] = BLANK
+                    # print current_row
+                    flag = True
+                    break
+                else:
+                    # print "Counter not equals constrains"
+                    for a in range(0, counter):
+                        # print "current row to delete is " + str(int(i + a))
+                        current_row[i + a] = BLANK
+                    # print current_row
+                    break
         if flag is False:
             violate += 1
-    print current_row
+
+    # check for any addition fill blank
+    for square in current_row:
+        if square is FILL:
+            # print 'square is filled'
+            violate += 1
 
     print violate
     return violate
@@ -72,4 +91,4 @@ def main():
 
 main()
 print_board()
-check_constraint(0)
+check_constraint(2)
