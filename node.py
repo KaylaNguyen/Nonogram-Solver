26ef32977d22df906443f39fbd2f7d8108fcfac1
Kayla Nguyen <nguye27k@mtholyoke.edu>
2016-04-15 14:18:03 -0400

    reformat files

--- node.py
+++ node.py
@@ -1,9 +1,7 @@
 class Node(object):
-	"""A node points to a state, the previous node in the search tree and the current depth"""
-	def __init__(self, state, prev_node, depth):
-		self.state = state
-		self.parent = prev_node
-		self.depth = depth 
+    """A node points to a state, the previous node in the search tree and the current depth"""
 
-
-		
+    def __init__(self, state, prev_node, depth):
+        self.state = state
+        self.parent = prev_node
+        self.depth = depth
