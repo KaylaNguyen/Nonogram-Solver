bfd6a51db6fdf7ae2e3a297d5aedc6a3fe8c54bb
Amna Aftab <amnaaftab94@gmail.com>
2016-04-24 19:07:38 -0400

    New Constraint checking

--- state.py
+++ state.py
@@ -4,9 +4,12 @@ class State(object):
     def __init__(self, row_constraints, column_constraints):
         self.row_constraints = row_constraints
         self.column_constraints = column_constraints
-        self.row_length = len(row_constraints)
-        self.col_length = len(column_constraints)
-        self.board = [["?" for x in range(self.row_length)] for y in range(self.col_length)]
+        self.row_length = len(row_constraints) # y
+        self.col_length = len(column_constraints) # x
+        self.board = [["?" for x in range(self.col_length)] for y in range(self.row_length)]
+        #print 'len(board): ', len(self.board)
+        #print 'len(board[0]): ', len(self.board[0])
+        #self.print_board()
         self.filledIndex = 0
 
     def get_board(self):
@@ -14,7 +17,7 @@ class State(object):
 
     def print_board(self):
         for i in self.board:
-            print i
+            print ' '.join(i)
 
     def add_row(self, row):
         self.board[self.filledIndex] = row
@@ -30,6 +33,13 @@ class State(object):
     def to_string(self):
         s = ""
         for i in range(self.row_length):
-            s+=''.join(self.board[i])
+            for j in self.board[i]:
+                if j == '-':
+                    s+= ' '
+                else:
+                    s += j 
+                s += ' '
+            #s+=' '.join(self.board[i])
+            s+= '\n'
         return s
 
