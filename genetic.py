f6e4e47abd136d2a4cfdecc00c941532173a05e4
Kayla Nguyen <nguye27k@mtholyoke.edu>
2016-04-25 00:23:17 -0400

    complete 2nd approach of GA

--- genetic.py
+++ genetic.py
@@ -12,7 +12,7 @@ class GeneticAlgorithm(object):
     global file
     file = open('output.txt', 'w')
     # instance of nonogram board
-    global nonogram, population, fitness, ROWS, COLUMNS, ROW_COUNT, COLUMN_COUNT, POPSIZE, PROBABILITY
+    global nonogram, population, fitness, ROWS, COLUMNS, ROW_COUNT, COLUMN_COUNT, POPSIZE, MUTATIONPROB, CROSSOVERPROB
     nonogram = Nonogram()
     population = []
     fitness = []
@@ -22,8 +22,9 @@ class GeneticAlgorithm(object):
     ROW_COUNT = len(ROWS)
     COLUMN_COUNT = len(COLUMNS)
 
-    POPSIZE = 100
-    PROBABILITY = 20
+    POPSIZE = 20
+    MUTATIONPROB = 20
+    CROSSOVERPROB = 75
 
     # generate random population of suitable solutions for nonogram
     def generate_population(self):
@@ -42,9 +43,10 @@ class GeneticAlgorithm(object):
             fit.append(nonogram.check_all_col(sol))
         return fit
 
+    '''1ST APPROACH'''
     # create a new population by repeating
     # selection, crossover, mutation, accepting
-    def create_new_population(self, pairs):
+    def create_new_population_1(self, pairs):
         pop = []
         # create a new population by repeating
         # selection, crossover, mutation, accepting (placing new offspring in new pop)
@@ -63,6 +65,95 @@ class GeneticAlgorithm(object):
 
         # return new generated population
         return pop
+
+    '''2ND APPROACH'''
+    # create a new population by repeating
+    # selection, crossover, mutation, accepting
+    def create_new_population_2(self, pairs):
+        pre_pop = []
+        # create a new population by repeating
+        # selection, crossover, mutation, accepting (placing new offspring in new pop)
+
+        # select best individuals based on their fitness
+        chosen = self.selection_2(pairs)
+        chosen = sorted(chosen, key=operator.attrgetter('fitness'))
+        pre_pop.append(chosen[0].get_state())
+        pre_pop.append(chosen[1].get_state())
+        # the rest crossover
+        cross = self.crossover_2(chosen[2:])
+        pre_pop.extend(cross)
+
+        # print pre_pop
+
+        # mutate the whole pre_pop
+        post_pop = []
+        for i in range(0, len(pre_pop)):
+            offspring = self.mutation(pre_pop[i])
+            # place new offspring in a new population
+            post_pop.append(offspring)
+        # return new generated population
+        return post_pop
+
+    # use roulette wheel to select best solutions from a population according to their fitness
+    # the better fitness, the bigger chance to be selected
+    def selection_2(self, pairs):
+        sum_fit = 0
+        for i in pairs:
+            sum_fit += i.get_fit()
+        # print "sum fit is", sum_fit
+        chosen = []
+        fitness_function = []
+        index = 0
+        for j in pairs:
+            fit_val = j.get_fit()
+            # print "fit val is", fit_val
+            fitness_function.append(range(index, index + fit_val))
+            index += fit_val
+        # print fitness_function
+
+        for k in range(0, POPSIZE):
+            probability = random.randint(0, sum_fit)
+            for a in range(0, len(fitness_function)):
+                if probability in fitness_function[a]:
+                    chosen.append(pairs[a])
+        # print len(chosen)
+        # print chosen
+        return chosen
+
+    # with a crossover probability cross over the parents to form 2 new offsprings
+    # return a list of new offsprings
+    def crossover_2(self, parents):
+        index_i = 0
+        index_j = 1
+        # list of offsprings to return
+        list = []
+        while index_i < len(parents) and index_j < len(parents):
+            # generate a random probability
+            probability = random.randint(0, 100)
+            if probability < CROSSOVERPROB:
+                # generate a random crossover point
+                crossover_point = random.randint(0, ROW_COUNT)
+                # copy everything before this point from parent 1 and after this point from parent 2
+                offspring1 = []
+                offspring2 = []
+                # get states of parents
+                state1 = parents[index_i].get_state()
+                state2 = parents[index_j].get_state()
+
+                for i in range(0, crossover_point):
+                    offspring1.append(state1[i])
+                    offspring2.append(state2[i])
+                for j in range(crossover_point, ROW_COUNT):
+                    offspring1.append(state2[j])
+                    offspring2.append(state1[j])
+                list.append(offspring1)
+                list.append(offspring2)
+            else:
+                list.append(parents[index_i].get_state())
+                list.append(parents[index_j].get_state())
+            index_i += 2
+            index_j += 2
+        return list
 
     # select 2 solutions from a population according to their fitness
     # the better fitness, the bigger chance to be selected
@@ -95,7 +186,7 @@ class GeneticAlgorithm(object):
         for i in range(0, ROW_COUNT):
             # 20% chance a row get mutated
             probability = random.randint(0, 100)
-            if probability <= PROBABILITY:
+            if probability <= MUTATIONPROB:
                 # print "row" + str(i) + " get mutated"
                 # print offspring[i]
                 rand = nonogram.get_random_row(i)
@@ -144,7 +235,7 @@ class GeneticAlgorithm(object):
             pairs = self.pair_up(population, fitness)
 
             # STEP 3: create a new population
-            population = self.create_new_population(pairs)
+            population = self.create_new_population_2(pairs)
 
             # STEP 4: check if the end condition is satisfied
             flag = self.check_goal(population)
@@ -162,10 +253,10 @@ class GeneticAlgorithm(object):
 
 '''
 in selection
+    roulette wheel from 20 with highest prob into another 20
     choose 2 best sol into next gen
-    then choose the rest by randomly choosing the sol with highest prob
-    using roulette wheel
-then crossover next gen
+then crossover 18 left
+mutate the whole 20
 '''
 
 GeneticAlgorithm()
